15da4b6fb53be7e1f4beb20f91153090057891e0
tom318267 <ogar318267@gmail.com>
2023-12-31 16:48:17 -0500

    adjust search filter code

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -51,27 +51,18 @@ const SearchManufacturer = ({
             afterLeave={() => setQuery("")}
           >
             <Combobox.Options>
-              {filteredManufacturers.length === 0 && query !== "" ? (
+              {filteredManufacturers.map((item) => (
                 <Combobox.Option
-                  value={query}
-                  className="search-manufacturer__option"
-                >
-                  Create "{query}"
-                </Combobox.Option>
-              ) : (
-                filteredManufacturers.map((item) => (
-                  <Combobox.Option
-                    key={item}
-                    className={({ active }) => `
+                  key={item}
+                  className={({ active }) => `
                     relative search-manufacturer__option
                     ${active ? "bg-primary-blue text white" : "text-gray-900"}
                     `}
-                    value={item}
-                  >
-                    {item}
-                  </Combobox.Option>
-                ))
-              )}
+                  value={item}
+                >
+                  {item}
+                </Combobox.Option>
+              ))}
             </Combobox.Options>
           </Transition>
         </div>
