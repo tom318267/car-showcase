9cb7b08805f05782294ebcc9850a647a2c227726
tom318267 <ogar318267@gmail.com>
2024-01-02 23:19:56 -0500

    add car card

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -60,7 +60,26 @@ const SearchManufacturer = ({
                     `}
                   value={item}
                 >
-                  {item}
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {item}
+                      </span>
+
+                      {/* Show an active blue background color if the option is selected */}
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-pribg-primary-purple"
+                          }`}
+                        ></span>
+                      ) : null}
+                    </>
+                  )}
                 </Combobox.Option>
               ))}
             </Combobox.Options>
